98be535d34ab991a1ddae017e47b10b566b0b908
Riya Deb <riyadeb.work@gmail.com>
2025-04-18 11:34:39 +0530

    new blog update

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// function myMessage(msg) {
+//     alert(msg);
+// }
+
+// // myMessage("Riya");
+
+// function mul(a,b) {
+//     let c = a*b;
+//     console.log("Product of "+a+" and "+b+" is "+c);
+// }
+
+// mul(15,45);
+
+// //document.body.style.backgroundColor = "red";
+
+// setTimeout(function(){
+    
+// },5000);
+
+// let a = 0;
+// document.querySelector('#myBtn').onclick = function() {
+//     console.log(++a);
+// };
+
+// Swal.fire({
+//     title: "Welcome to my Shopping Page",
+//     icon: "success",
+//     showConfirmButton: false,
+//     timer: 2000
+// });
+
+// console.log(new Date().getDate());
